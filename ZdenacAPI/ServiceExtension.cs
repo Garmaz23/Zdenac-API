c2d745a4c50d925e832563d03c930b2f7445a4de
Ivan Garmaz <garmaz.ivan@gmail.com>
2023-08-09 22:03:33 +0200

    added GEH, but without logging implemented

---
+++ ZdenacAPI/ServiceExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Zdenac_API
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(error =>
+            {
+                error.Run(async context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        // Log.Error($"Something went wrong in the {contextFeature.Error}");
+                        await context.Response.WriteAsync(new Error
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal server error. Please try again later."
+                        }.ToString());
+                    }
+
+                });
+            });
+        }
+    }
+}
