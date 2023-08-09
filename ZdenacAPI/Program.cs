193a65129d1bd73043698701902137892c6616a0
Ivan Garmaz <garmaz.ivan@gmail.com>
2023-08-09 23:08:48 +0200

    fix

--- ZdenacAPI/Program.cs
+++ ZdenacAPI/Program.cs
@@ -24,6 +24,7 @@ var logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
     .CreateLogger();
+
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
 
