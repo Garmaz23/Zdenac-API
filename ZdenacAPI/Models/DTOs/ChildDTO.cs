7161fbc88c4f58fd19cb1e838c99919708739a61
abartulovic <ante.bartulovic2@skole.hr>
2023-08-09 02:23:07 +0200

    Changed ChildDTO

--- ZdenacAPI/Models/DTOs/ChildDTO.cs
+++ ZdenacAPI/Models/DTOs/ChildDTO.cs
@@ -5,40 +5,23 @@ namespace Zdenac_API.Models.DTOs
     public class ChildDTO
     {
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(50)]
         public string FirstName { get; set; }
-
-        [Required]
-        [StringLength(50)]
         public string LastName { get; set; }
-
         public DateTime BirthDate { get; set; }
-
-        [Phone]
-        public string Mobile { get; set; }
-
+        public string PhoneNumber { get; set; }
         public int BirthLocationId { get; set; }
-
         public int GenderId { get; set; }
-
-        [EmailAddress]
         public string Email { get; set; }
-
         public int MaleSiblings { get; set; }
         public int FemaleSiblings { get; set; }
-
-        [StringLength(50)]
-        public string ParentFather { get; set; }
-
-        [StringLength(50)]
-        public string ParentMother { get; set; }
-
-        public int GuardianId { get; set; }
-
-        [StringLength(200)]
+        public DateTime CreatedAt { get; set; }
+        public bool Active { get; set; }
+        public int InstitutionId { get; set; }
         public string Remark { get; set; }
+
+        public Location BirthLocation { get; set; }
+        public Gender Gender { get; set; }
+        public Institution Institution { get; set; }
     }
 }
 
